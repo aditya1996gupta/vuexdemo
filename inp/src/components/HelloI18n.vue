eb9f2fdd70251cbe8aef3cc3018c984bf301bb96
aditya gupta <adityasgupta309@gmail.com>
2020-06-18 20:22:37 +0530

    vue internationalization

---
+++ inp/src/components/HelloI18n.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <p>{{ $t("message", { name }) }}</p>
+    <p>{{ $t("email",{email}) }}</p>
+    <p>{{ $t("goodbye") }}</p>
+    <button @click="setLocale('en')" class="button">English</button>
+    <button @click="setLocale('hn')" class="button">Hindi</button>
+    <button @click="setLocale('ma')" class="button">Marathi</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HelloI18n",
+  data() {
+    return {
+      name: "peter",
+      email: "info@learnsomething"
+    };
+  },
+  methods: {
+    setLocale(locale) {
+      this.$i18n.locale = locale;
+      localStorage.setItem("locale", locale);
+    }
+  }
+};
+</script>
