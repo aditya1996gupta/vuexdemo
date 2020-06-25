e4c07d3ce2c0ea8bb2def592d1e78cc54b0bf605
aditya gupta <adityasgupta309@gmail.com>
2020-06-25 18:55:35 +0530

    charts in vue

---
+++ chartjsdemo/src/views/Home.vue
@@ -0,0 +1,15 @@
+<template>
+  <section class="hero">
+    <div class="hero-body">
+      <div class="container">
+        <h1>Creating Charts Using Vue.js Wrappers For Chart.js</h1>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: "home"
+};
+</script>
